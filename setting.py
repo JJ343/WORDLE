8a5348de6c9d746010e62b5c786d4fc750422a0c
JJ343 <125293683+JJ343@users.noreply.github.com>
2023-11-13 21:30:20 -0500

    setting.py

--- setting.py
+++ setting.py
@@ -4,8 +4,11 @@ import button
 
 Black= (0,0,0)
 White=(255,255,255)
+Grey= (100, 100, 100)
+Green=(0, 255,0 )
+Yellow=(255, 255, 255)
 
-Ancho= 700
+Ancho= 800
 Alto = 700
 
 title= "wordle"
@@ -17,15 +20,14 @@ let_6_img= pygame.image.load("images/6 letras.png")
 let_7_img= pygame.image.load("images/7 letras.png")
 let_8_img= pygame.image.load("images/8 letras.png")
 
-button_4= button.Button (275,75, let_4_img,1)
-button_5= button.Button (275,175, let_5_img,1)
-button_6= button.Button (275,275, let_6_img,1)
-button_7= button.Button (275,375, let_7_img,1)
-button_8= button.Button (275,475, let_8_img,1)
+button_4= button.Button (300,75, let_4_img,1)
+button_5= button.Button (300,175, let_5_img,1)
+button_6= button.Button (300,275, let_6_img,1)
+button_7= button.Button (300,375, let_7_img,1)
+button_8= button.Button (300,475, let_8_img,1)
 
-TILESIZE =70
-GAPSIZE =10
-
+TILESIZE =80
+GAPSIZE =15
 
 
 
